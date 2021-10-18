4c3cf8f7d9b6d3ea7d908eefe7d608dfc6053817
vishal.Alok <you@example.com>
2021-10-18 08:39:08 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+public class ThreadExample
+{
+    public static void test1()
+    {
+        for(int i = 0; i<= 100; i++)
+        {
+            Console.WriteLine("test1 : " + i);
+        }
+    }
+    public static void test2()
+    {
+        for (int i = 0; i <= 100; i++)
+        {
+            Console.WriteLine("test2  : " + i);
+
+            if(i == 50)
+            {
+                Thread.Sleep(15000);
+            }
+        }
+    }
+    public static void test3()
+    {
+        for (int i = 0; i <= 100; i++)
+        {
+            Console.WriteLine("test3  : " + i);
+        }
+    }
+    public static void Main(string[] args)
+    {
+        Thread t1 = new Thread(test1);
+        Thread t2= new Thread(test2);
+        Thread t3 = new Thread(test3);
+        t1.Start();
+        t2.Start();
+        t3.Start();
+    }
+}
